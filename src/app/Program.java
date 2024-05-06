1c4367ea11158cff00315cf7bfa04037e7eb29d0
Natan Neves <dev.natanbarbosa@gmail.com>
2024-05-06 10:26:28 -0300

    DepartmentDao and SellerDao interfaces

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,22 +1,17 @@
 package app;
 
-import java.util.Date;
 import java.util.Locale;
 
-import model.entities.Department;
-import model.entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 public class Program {
 
 	public static void main(String[] args) {
-	
 		Locale.setDefault(Locale.US);
 		
-		Seller sel = new Seller(1, "natan", "natan@gmail.com", new Date(), 3000.0, new Department(1, "books"));
-		
-		
-
-		System.out.print(sel);
+	
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 	}
 
 }
--- src/app/application.java
+++
@@ -1,9 +0,0 @@
-package app;
-
-public class application {
-
-    public static void main(String[] args) {
-
-        System.out.println("meu nome é");
-    }
-}
